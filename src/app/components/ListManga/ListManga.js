05279dce876f76d754b515b9fc21196240ea4c38
Ryan Hill <ryan.hill@tilefive.com>
2020-05-20 05:26:21 -0500

    Real Information Being Shown!
    Query change

--- src/app/components/ListManga/ListManga.js
+++ src/app/components/ListManga/ListManga.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
 import * as mangaAPI from '../../../service/manga/api';
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  stretch: {
+    width: 75,
+    height: 100,
+    resizeMode: 'stretch',
+  },
+});
 
 class ListManga extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      data:[]
+      data:{
+        data:[]
+      },
+      loading: true,
     }
   }
 
   componentDidMount() {
-    mangaAPI.search("mangareader").then(
-      ({ data }) => console.log(data)
-      // ({ data }) => this.setState({ data })
+    this.search();
+  }
+
+  search = async () => {
+    await mangaAPI.search("mangareader", "page=2").then(
+      ({ data }) => this.setState({ loading: false, data })
     );
     console.log(this.state.data);
   }
 
   render() {
-    return (
-      <Text>Test</Text>
+
+    const Manga = this.state.data.data.map((manga)=>{
+      return (
+        <View>
+          <Text>{manga.name}</Text>
+          <Text>{manga.chapters}</Text>
+          <Text>{manga.genre}</Text>
+          <Image style={styles.stretch} source={{uri: manga.thumb}} />
+        </View>
+      )
+    });
+
+    return !this.state.loading ? (
+      <View>
+      <Text>{this.state.loading || ""}</Text>
+      <Button
+        onPress={this.search}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button" />
+      { Manga }
+      </View>
+    ) : (
+      <Text> Loading... </Text>
     )
   }
 }
