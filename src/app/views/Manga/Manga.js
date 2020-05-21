7a18bf706cb9c8811dedabe908276c2251c8f00f
Ryan Hill <ryan.hill@tilefive.com>
2020-05-20 23:13:36 -0500

    That wasn't bad

--- src/app/views/Manga/Manga.js
+++ src/app/views/Manga/Manga.js
@@ -1,13 +1,13 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
-export default function Manga() {
+export default function Manga({navigation, route}) {
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
+        <Text>{route.params.manga}</Text>
       </ScrollView>
     </View>
   );
