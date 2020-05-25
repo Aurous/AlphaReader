8e5798642f63d364b100fc32263e2808363a81e2
Ryan Hill <ryan.hill@tilefive.com>
2020-05-25 02:42:44 -0500

    fuck

--- server/api/_lib/manga/mangareader/service.mjs
+++ server/api/_lib/manga/mangareader/service.mjs
@@ -49,11 +49,6 @@ class MangaReader extends SourceService {
             search.name  = _(this).find('a').text();
             search.url = '/chapters/mangareader' + _(this).find('a').attr('href');
             search.thumb = _(this).find('.imgsearchresults').css('background-image').replace('url(\'','').replace('\')','').replace('r0','l0');
-            _(this).find('.propertytitle').each((item) => {
-              if(item.text() === "Reading Direction:"){
-                console.log(item.last().text());
-              }
-            });
             search.chapters = _(this).find('.chapter_count').text();
             search.genre = _(this).find('.manga_genre').text();
             data.push(search);
