09023291c6f0d7667740202f43cd16f2c54aaced
Ryan Hill <ryan.hill@tilefive.com>
2020-05-25 02:36:55 -0500

    log

--- server/api/_lib/manga/mangareader/service.mjs
+++ server/api/_lib/manga/mangareader/service.mjs
@@ -49,8 +49,11 @@ class MangaReader extends SourceService {
             search.name  = _(this).find('a').text();
             search.url = '/chapters/mangareader' + _(this).find('a').attr('href');
             search.thumb = _(this).find('.imgsearchresults').css('background-image').replace('url(\'','').replace('\')','').replace('r0','l0');
-            search.testing = _(this).find('.propertytitle').parent().each((item) => { 
-              if(item.text() === "Reading Direction:") return item.last().text();
+            search.testing = _(this).find('.propertytitle').each((item) => {
+              if(item.text() === "Reading Direction:"){
+                console.log(item.last().text());
+                return item.last().text();
+              }
             });
             search.chapters = _(this).find('.chapter_count').text();
             search.genre = _(this).find('.manga_genre').text();
