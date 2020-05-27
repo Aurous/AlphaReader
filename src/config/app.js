041bffddde839a1ee39026876489c61ab016ab37
Ryan Hill <ryan.hill@tilefive.com>
2020-05-27 02:04:55 -0500

    dev local

--- src/config/app.js
+++ src/config/app.js
@@ -1,11 +1,10 @@
 const APP_CONFIG = {
   localhost: {
-    // apiRoot: 'https://ryt7pxlc5h.execute-api.us-east-1.amazonaws.com/testing/account',
-    apiRoot: 'https://manga.ryanhill.com',
-  },
-  production: {
     apiRoot: 'http://localhost:3000',
   },
+  production: {
+    apiRoot: 'https://manga.ryanhill.com',
+  }
 };
 
 const getAppConfig = () => {
